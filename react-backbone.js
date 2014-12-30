cdf72dcf8efde4c0e919545c88f7f33ea6c7b5a9
Joe Hudson <joehud@gmail.com>
2014-12-30 10:17:01 -0500

    v0.16.0

--- react-backbone.js
+++ react-backbone.js
@@ -1,5 +1,5 @@
 /*!
- * react-backbone v0.15.0
+ * react-backbone v0.16.0
  * https://github.com/jhudson8/react-backbone
  *
  * Copyright (c) 2014 Joe Hudson<joehud_AT_gmail.com>
--- with-deps.js
+++ with-deps.js
@@ -28,7 +28,7 @@
     jhudson8/backbone-xhr-events 0.9.5
     jhudson8/react-mixin-manager 0.10.0
     jhudson8/react-events 0.7.9
-    jhudson8/react-backbone 0.15.0
+    jhudson8/react-backbone 0.16.0
 */
  (function(main) {
   if (typeof define === 'function' && define.amd) {
