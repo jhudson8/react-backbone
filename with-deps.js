7dc7ad32b7556414dd5b210dd951b8a7f4f9ca6f
Joe Hudson <joehud@gmail.com>
2015-02-22 10:42:50 -0500

    v0.21.1

--- with-deps.js
+++ with-deps.js
@@ -28,7 +28,7 @@
     jhudson8/backbone-xhr-events 0.11.2
     jhudson8/react-mixin-manager 0.13.0
     jhudson8/react-events 0.9.0
-    jhudson8/react-backbone 0.21.0
+    jhudson8/react-backbone 0.21.1
 */
  (function(main) {
   if (typeof define === 'function' && define.amd) {
